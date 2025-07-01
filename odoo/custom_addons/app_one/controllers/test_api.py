cec6aebbc14f903f31a5346d729eefbff40a963e
Isaladwy <Isaladwy@gmail.com>
2025-07-01 10:36:28 +0300

    Refactor TestApi controller to use HTTP type and enhance response structure

--- odoo/custom_addons/app_one/controllers/test_api.py
+++ odoo/custom_addons/app_one/controllers/test_api.py
@@ -2,4 +2,11 @@ from odoo import http
 
 
 class TestApi(http.Controller):
-    @http.route(route="/api/test", type="json", auth="none", methods=["GET"])
+    @http.route(route="/api/test", type="http", auth="none", csrf=False, methods=["GET"])
+    def test_api(self):
+        print("Test API called")
+        return http.Response(
+            status=200,
+            content_type="application/json",
+            response='{"message": "Test API response"}',
+        )
