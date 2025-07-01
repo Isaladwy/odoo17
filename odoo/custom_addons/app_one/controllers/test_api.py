250b0b361e4b262beca0944ccd628c76ed5c15de
Isaladwy <Isaladwy@gmail.com>
2025-07-01 11:04:14 +0300

    Refactor test_api method to return response instead of printing

--- odoo/custom_addons/app_one/controllers/test_api.py
+++ odoo/custom_addons/app_one/controllers/test_api.py
@@ -4,5 +4,5 @@ from odoo import http
 class TestApi(http.Controller):
     @http.route(route="/api/test", type="http", auth="none", csrf=False, methods=["GET"])
     def test_api(self):
-        print("Test API called")
+        return "Test API called"
        
